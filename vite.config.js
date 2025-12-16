1bfd85fd380c2807728aec4319d3f6dd40438bc2
imjoe77 <nathanielbandi636@gmail.com>
2025-12-16 09:04:19 +0530

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,4 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/CypherVault_Local/',
-})
+  base: "/CypherVault_Local/",
+});
